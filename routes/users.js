3e65952ac0cb8533bfaa24a10da55904242b3a7d
John-Michael Riley <johnmichael.w.riley@gmail.com>
2020-12-01 12:12:54 -0500

    add user register route

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/', async (req, res) => {
+  createUser();
+  req.session.user = user;
+  req.session.save((err) => {
+    return res.redirect('/');
+  });
 });
 
 module.exports = router;
